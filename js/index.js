f639889dfff501e539f96ed18d034b94181e7a42
NikMg <example@gmail.com>
2024-12-08 20:02:09 +0200

    first comit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const storeName = 'sexshop';
+const storeDescription = {
+    budget : 10000,
+    employees : ['alex', 'ivan', 'john'],
+    products : {
+        'pensil' : 22,
+        'lamp' : 11
+    },
+    open : true
+    
+}
+console.log(storeDescription.products);
